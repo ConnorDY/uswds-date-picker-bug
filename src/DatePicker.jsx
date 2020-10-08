9c2a3ef09082251c8b53567a633e0a30587c04db
Connor Younglund <younglund_connor@bah.com>
2020-10-08 17:33:06 -0400

    try calling USWDS's date picker `on` function (also fix attribute names)

--- src/DatePicker.jsx
+++ src/DatePicker.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import datePicker from 'uswds/src/js/components/date-picker';
 
 function DatePicker() {
+  const datePickerRef = useRef();
+
+  useEffect(() => {
+    const datePickerEl = datePickerRef.current;
+    if (datePickerEl) datePicker.on(datePickerEl);
+  }, [datePickerRef]);
+
   return (
     <form className="usa-form">
-      <div class="usa-form-group">
+      <div className="usa-form-group">
         <label
-          class="usa-label"
+          className="usa-label"
           id="appointment-date-label"
-          for="appointment-date"
+          htmlFor="appointment-date"
         >
           Appointment date
         </label>
 
-        <div class="usa-hint" id="appointment-date-hint">
+        <div className="usa-hint" id="appointment-date-hint">
           mm/dd/yyyy
         </div>
 
-        <div class="usa-date-picker">
+        <div className="usa-date-picker" ref={datePickerRef}>
           <input
-            class="usa-input"
+            className="usa-input"
             id="appointment-date"
             name="appointment-date"
             type="text"
